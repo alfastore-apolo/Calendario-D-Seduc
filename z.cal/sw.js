93ba27cbcecca83c6fcb9d58af5e5d12942daed4
alfastore-apolo <corcursopublico@gmail.com>
2026-05-23 12:20:48 -0400

    primeiro commit

---
+++ z.cal/sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'calendario-seduc-v1';
+const ASSETS = [
+  '/Calendario-D-Seduc/',
+  '/Calendario-D-Seduc/index.html',
+  '/Calendario-D-Seduc/manifest.json'
+];
+
+// Instala e faz cache dos arquivos principais
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Limpa caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Tenta rede primeiro, cai no cache se offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
